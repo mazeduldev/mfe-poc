b1f3c325b48ea30b5af58e6db21e65a595135cb8
Mazedul Islam <mazidmailbox@gmail.com>
2025-10-30 10:44:05 +0600

    fix: set a base path for docs project

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,6 +2,7 @@ import { withMicrofrontends } from "@vercel/microfrontends/next/config";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   turbopack: {},
+  basePath: "/docs",
 };
 
 export default withMicrofrontends(nextConfig);
